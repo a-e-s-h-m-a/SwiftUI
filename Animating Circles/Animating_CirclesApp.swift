f8b676271ed2454204b074848adaeb013f2edb88
Aeshma <madhushanka2727@gmail.com>
2023-04-09 11:19:50 +0530

    Initial Commit

---
+++ Animating Circles/Animating_CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_CirclesApp.swift
+//  Animating Circles
+//
+//  Created by Admin on 2023-04-09.
+//
+
+import SwiftUI
+
+@main
+struct Animating_CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
